601adb5ec7d5d7364d899981b84320669f43954c
lalit760 <sirlalit1tr@gmail.com>
2026-07-16 11:31:19 +0530

    Time: 2 ms (58.79%), Space: 14.2 MB (58.47%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void helper(vector<int>& cad, int t, int idx,vector<int>&subset){
+        if(t==0){
+            result.push_back(subset);
+            return;
+        }
+        if(idx==cad.size()) return;
+        if(cad[idx] <= t){
+            subset.push_back(cad[idx]); //pick & stay
+            helper(cad,t-cad[idx],idx,subset);
+            subset.pop_back();
+
+            // subset.push_back(cad[idx]); //pick & move
+            // helper(cad,t-cad[idx],idx+1,subset);
+            // subset.pop_back();
+        }
+        helper(cad,t,idx+1,subset); //not pick & move
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> subset;
+        sort(candidates.begin(),candidates.end());
+        helper(candidates, target,0, subset);
+        return result;
+    }
+};
