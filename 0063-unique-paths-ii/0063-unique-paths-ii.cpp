f0c643806ae5e8ae840b92fefbc7eb0e54cd1447
lalit760 <sirlalit1tr@gmail.com>
2026-07-18 11:49:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (6.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<bool>>obstacle(m,vector<bool>(n,true));
-        vector<vector<long long>>dp(m,vector<long long>(n,0));
+        vector<vector<long long>>dp(m,vector<long long>(n,0)); //have to use long long
 
         if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0; //special case
         dp[m-1][n-1] = 1;       
@@ -14,7 +14,7 @@ public:
             if(obstacleGrid[i][n-1] == 1) break;
             dp[i][n-1]= dp[i+1][n-1];
         }
-        
+
         // for last row
         for(int j=n-2;j>=0;j--){
             if(obstacleGrid[m-1][j] == 1) break;
