d9b295de015c95784fc0c8b235c677bfe144f4a5
lalit760 <sirlalit1tr@gmail.com>
2026-07-18 11:49:41 +0530

    Time: 1 ms (15.84%), Space: 12.2 MB (6.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,31 +6,24 @@ public:
         vector<vector<bool>>obstacle(m,vector<bool>(n,true));
         vector<vector<long long>>dp(m,vector<long long>(n,0));
 
-        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0;
-        dp[m-1][n-1] = 1;
+        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0; //special case
+        dp[m-1][n-1] = 1;       
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j] == 1){
-                    obstacle[i][j] = false;
-                }
-            }
-        }
-        // if(obstacle[m-1][n-1] == false || obstacle[0][0] == false) return 0;
-        
-
+        // for last column
         for(int i=m-2;i>=0;i--){
-            if(!obstacle[i][n-1]) break;
+            if(obstacleGrid[i][n-1] == 1) break;
             dp[i][n-1]= dp[i+1][n-1];
         }
+        
+        // for last row
         for(int j=n-2;j>=0;j--){
-            if(!obstacle[m-1][j]) break;
+            if(obstacleGrid[m-1][j] == 1) break;
             dp[m-1][j]=dp[m-1][j+1];
         }
 
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                if(obstacle[i][j]){
+                if(obstacleGrid[i][j] == 0){ //ignoring obstacles
                     dp[i][j] = dp[i][j+1] + dp[i+1][j];
                 }
             }
