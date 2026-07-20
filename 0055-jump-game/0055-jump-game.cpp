4f0004a99499701fcc7a8dcd8ac3f238ead7475e
lalit760 <sirlalit1tr@gmail.com>
2026-07-20 11:09:55 +0530

    Time: 0 ms (100%), Space: 52.5 MB (17.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        // BOTTOM-UP DP
         int n = nums.size();
-        vector<bool> dp(n, false);
-        dp[n-1] = true;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] != 0){
-                for(int j=1;j<=nums[i];j++){
-                    if(dp[i+j]==true){
-                        dp[i] = true;
-                        break;
-                    }
-                }
-            }   
+        // vector<bool> dp(n, false);
+        // dp[n-1] = true;
+        // for(int i=n-2;i>=0;i--){
+        //     if(nums[i] != 0){
+        //         for(int j=1;j<=nums[i];j++){
+        //             if(dp[i+j]==true){
+        //                 dp[i] = true; break;
+        //             }
+        //         }
+        //     }   
+        // }
+        // return dp[0];
+
+        //Greedy
+        int goal = n-1;
+        for(int i=n-2;i>=0;i--){ //travelling form 2nd last to oth index
+            if(i + nums[i] >= goal){ //checking if it is possilbe to achieve goal index from that index
+                goal = i;
+            }
         }
-        return dp[0];
+        return goal==0;
     }
 };
