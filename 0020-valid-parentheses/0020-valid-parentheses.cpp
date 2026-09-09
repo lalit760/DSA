fe4802a7b6f68cfbd70b2ebceb500a4d7a455d2e
lalit760 <sirlalit1tr@gmail.com>
2026-09-09 20:18:05 +0530

    Time: 3 ms (9.23%), Space: 10.1 MB (6.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        int n=s.size();
+        st.push(s[0]);
+        for(int i=1;i<n;i++){
+            if(!st.empty() && st.top()=='(' && s[i]==')'){
+                st.pop();
+            }
+            else if(!st.empty() && st.top()=='{' && s[i]=='}'){
+                st.pop();
+            }
+            else if(!st.empty() && st.top()=='[' && s[i]==']'){
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+        if(st.empty())return true;
+        return false;
+    }
+};
